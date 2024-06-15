fa85e769c25a0d9267313deb7e2b72713184c794
huynamboz <teahuynam2003@gmail.com>
2024-06-16 02:40:23 +0700

    feat: remove sth

--- src/views/Dashboard/ActivityView.vue
+++ src/views/Dashboard/ActivityView.vue
@@ -122,7 +122,7 @@ const timeoutVolume = ref<any>(null)
       <div class="mt-4 grid grid-cols-2 max-md:grid-cols-1 gap-3">
         <!-- left -->
         <div class="grid grid-cols-2 gap-3">
-          <h4 class="text-base font-base text-black dark:text-white">Chế độ cảnh báo{{ systemStatus }}</h4>
+          <h4 class="text-base font-base text-black dark:text-white">Chế độ cảnh báo</h4>
           <SwitchFour @change="onChangeSystem" v-model="systemStatus"/>
           <h4 class="text-base font-base text-black dark:text-white">Còi</h4>
           <SwitchFour @update:modelValue="onChangeSystem" :disabled="true" v-model="masterStore.buzzer"/>
